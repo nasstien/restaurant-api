3c00d67ef3718f70deafb7a08bc99d680561ed18
Anastasia <vsrt3835@gmail.com>
2024-07-14 12:13:09 +0300

    rename routes

--- src/routes/stats.routes.ts
+++ src/routes/stats.routes.ts
@@ -14,8 +14,8 @@ const router = Router();
 
 router.use(protect, checkUserRole(Role.admin));
 
-router.get('/user-count-by-role', getUserCountByRole);
-router.get('/user-count-by-activity', getUserCountByActivity);
+router.get('/users/role', getUserCountByRole);
+router.get('/users/activity', getUserCountByActivity);
 router.get('/income-per-month/:month', getIncomePerMonth);
 router.get('/top-selling-items', getTopSellingItems);
 router.get('/most-reserved-tables', getMostReservedTables);
